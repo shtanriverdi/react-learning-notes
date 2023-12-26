9d579a2d6bcd05cc2947a07b77ac4cff5210af23
shtanriverdi <shtanriverdi@yahoo.com>
2023-12-26 13:56:28 +0300

    Lifting State Up Again

--- src/TicTacToe/Board.jsx
+++ src/TicTacToe/Board.jsx
@@ -1,61 +1,12 @@
-import { useState } from "react";
 import Square from "./Square";
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [gameDone, setGameDone] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [winnerColors, setWinnerColors] = useState(Array(9).fill("black"));
-
-  const getNextPlayer = () => {
-    return xIsNext ? "X" : "O";
-  };
-
-  // Game State
-  let status;
-  const [winner, winnerSquares] = calculateWinner(squares);
-  if (winner) {
-    status = "Winner is " + winner;
-    if (gameDone) {
-      setWinnerColors((prevWinnerColors) => {
-        const newWinnerColor = [...prevWinnerColors];
-        for (const winnerSquare of winnerSquares) {
-          newWinnerColor[winnerSquare] = "green";
-        }
-        return newWinnerColor;
-      });
-      setGameDone(false);
-    }
-  } else if (!winner && !squares.some((cell) => cell === null)) {
-    status = "Game is a tie";
-  } else {
-    status = "Next player: " + getNextPlayer();
-  }
-
-  const handleClick = (i) => {
-    // If the square is already 'X' or 'O' (clicked)
-    // or There is a winner either 'X' or 'O'
-    // Don't continue
-    const winner = calculateWinner(squares)[0];
-    if (squares[i] || winner) {
-      return;
-    }
-    // Treating squares array as immutable
-    // This will allow you to store every past version of the squares array
-    // and navigate between the turns that have already happened.
-    const newSquares = squares.slice();
-    // Alternative way
-    // const newSquares = [ ...squares ];
-    newSquares[i] = getNextPlayer();
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  };
-
-  const handleReset = () => {
-    setSquares(Array(9).fill(null));
-    setGameDone(true);
-    setWinnerColors(Array(9).fill('black'));
-  };
+export default function Board({
+  status,
+  squares,
+  winnerColors,
+  onReset,
+  onSquareClick
+}) {
 
   return (
     <>
@@ -64,77 +15,56 @@ export default function Board() {
         <Square
           color={winnerColors[0]}
           value={squares[0]}
-          onSquareClick={() => handleClick(0)}
+          onSquareClick={() => onSquareClick(0)}
         />
         <Square
           color={winnerColors[1]}
           value={squares[1]}
-          onSquareClick={() => handleClick(1)}
+          onSquareClick={() => onSquareClick(1)}
         />
         <Square
           color={winnerColors[2]}
           value={squares[2]}
-          onSquareClick={() => handleClick(2)}
+          onSquareClick={() => onSquareClick(2)}
         />
       </div>
       <div className="board-row">
         <Square
           color={winnerColors[3]}
           value={squares[3]}
-          onSquareClick={() => handleClick(3)}
+          onSquareClick={() => onSquareClick(3)}
         />
         <Square
           color={winnerColors[4]}
           value={squares[4]}
-          onSquareClick={() => handleClick(4)}
+          onSquareClick={() => onSquareClick(4)}
         />
         <Square
           color={winnerColors[5]}
           value={squares[5]}
-          onSquareClick={() => handleClick(5)}
+          onSquareClick={() => onSquareClick(5)}
         />
       </div>
       <div className="board-row">
         <Square
           color={winnerColors[6]}
           value={squares[6]}
-          onSquareClick={() => handleClick(6)}
+          onSquareClick={() => onSquareClick(6)}
         />
         <Square
           color={winnerColors[7]}
           value={squares[7]}
-          onSquareClick={() => handleClick(7)}
+          onSquareClick={() => onSquareClick(7)}
         />
         <Square
           color={winnerColors[8]}
           value={squares[8]}
-          onSquareClick={() => handleClick(8)}
+          onSquareClick={() => onSquareClick(8)}
         />
       </div>
-      <button className="btn" onClick={handleReset}>Reset Game</button>
+      <button className="btn" onClick={onReset}>
+        Reset Game
+      </button>
     </>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  // Winner found
-  for (const [a, b, c] of lines) {
-    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-      return [squares[a], [a, b, c]];
-    }
-  }
-
-  // No winner
-  return [null, null];
 }
--- src/TicTacToe/Game.jsx
+++ src/TicTacToe/Game.jsx
@@ -1,12 +1,76 @@
+import { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
+  // History State
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [gameDone, setGameDone] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winnerColors, setWinnerColors] = useState(Array(9).fill("black"));
+
+  const getNextPlayer = () => {
+    return xIsNext ? "X" : "O";
+  };
+
+  // Game State
+  let status;
+  const [winner, winnerSquares] = calculateWinner(squares);
+  if (winner) {
+    status = "Winner is " + winner;
+    if (gameDone) {
+      setWinnerColors((prevWinnerColors) => {
+        const newWinnerColor = [...prevWinnerColors];
+        for (const winnerSquare of winnerSquares) {
+          newWinnerColor[winnerSquare] = "green";
+        }
+        return newWinnerColor;
+      });
+      setGameDone(false);
+    }
+  } else if (!winner && !squares.some((cell) => cell === null)) {
+    status = "Game is a tie";
+  } else {
+    status = "Next player: " + getNextPlayer();
+  }
+
+  const handleClick = (i) => {
+    // If the square is already 'X' or 'O' (clicked)
+    // or There is a winner either 'X' or 'O'
+    // Don't continue
+    const winner = calculateWinner(squares)[0];
+    if (squares[i] || winner) {
+      return;
+    }
+    // Treating squares array as immutable
+    // This will allow you to store every past version of the squares array
+    // and navigate between the turns that have already happened.
+    const newSquares = squares.slice();
+    // Alternative way
+    // const newSquares = [ ...squares ];
+    newSquares[i] = getNextPlayer();
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const handleReset = () => {
+    setSquares(Array(9).fill(null));
+    setGameDone(true);
+    setWinnerColors(Array(9).fill("black"));
+  };
+
   return (
     <div className="container">
       <h1>TicTacToe Tutorial</h1>
       <div className="game flex">
         <div className="game-board flex">
-          <Board />
+          <Board
+            squares={squares}
+            winnerColors={winnerColors}
+            status={status}
+            onReset={handleReset}
+            onSquareClick={handleClick}
+          />
         </div>
 
         <div className="game-info flex">
@@ -20,4 +84,27 @@ export default function Game() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // Winner found
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return [squares[a], [a, b, c]];
+    }
+  }
+
+  // No winner
+  return [null, null];
 }
