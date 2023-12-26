ee8292d8e3187b44f545d7555e9cc30a88af222d
shtanriverdi <shtanriverdi@yahoo.com>
2023-12-27 00:03:35 +0300

    Clean up code

--- src/TicTacToe/Game.jsx
+++ src/TicTacToe/Game.jsx
@@ -4,9 +4,11 @@ import Board from "./Board";
 export default function Game() {
   // History State
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
   const [gameDone, setGameDone] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
   const [winnerColors, setWinnerColors] = useState(Array(9).fill("black"));
 
   const getNextPlayer = () => {
@@ -15,7 +17,7 @@ export default function Game() {
 
   // Game State
   let status;
-  const [winner, winnerSquares] = calculateWinner(squares);
+  const [winner, winnerSquares] = calculateWinner(currentSquares);
   if (winner) {
     status = "Winner is " + winner;
     if (gameDone) {
@@ -28,7 +30,7 @@ export default function Game() {
       });
       setGameDone(false);
     }
-  } else if (!winner && !squares.some((cell) => cell === null)) {
+  } else if (!winner && !currentSquares.some((cell) => cell === null)) {
     status = "Game is a tie";
   } else {
     status = "Next player: " + getNextPlayer();
@@ -38,26 +40,49 @@ export default function Game() {
     // If the square is already 'X' or 'O' (clicked)
     // or There is a winner either 'X' or 'O'
     // Don't continue
-    const winner = calculateWinner(squares)[0];
-    if (squares[i] || winner) {
+    const winner = calculateWinner(currentSquares)[0];
+    if (currentSquares[i] || winner) {
       return;
     }
     // Treating squares array as immutable
     // This will allow you to store every past version of the squares array
     // and navigate between the turns that have already happened.
-    const newSquares = squares.slice();
+    const newSquares = currentSquares.slice();
     // Alternative way
     // const newSquares = [ ...squares ];
     newSquares[i] = getNextPlayer();
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
+    // Store history
+    // Keep only portion of history up until currentMove
+    const nextHistory = [...history.slice(0, currentMove + 1), newSquares];
+    setHistory(nextHistory);
+    // Update currentMove to the latest history entry
+    setCurrentMove(nextHistory.length - 1);
   };
 
   const handleReset = () => {
-    setSquares(Array(9).fill(null));
     setGameDone(true);
     setWinnerColors(Array(9).fill("black"));
+    setHistory([Array(9).fill(null)]);
   };
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, index) => {
+    let description;
+    if (index > 0) {
+      description = "Go to move #" + index;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={index}>
+        <button onClick={() => jumpTo(index)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="container">
@@ -65,7 +90,7 @@ export default function Game() {
       <div className="game flex">
         <div className="game-board flex">
           <Board
-            squares={squares}
+            squares={currentSquares}
             winnerColors={winnerColors}
             status={status}
             onReset={handleReset}
@@ -74,12 +99,7 @@ export default function Game() {
         </div>
 
         <div className="game-info flex">
-          <ol>
-            <li>Test TicTacToeTicTacToe</li>
-            <li>Test TicTacToe</li>
-            <li>Test TicTacToe</li>
-            <li>Test TicTacToe</li>
-          </ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     </div>
