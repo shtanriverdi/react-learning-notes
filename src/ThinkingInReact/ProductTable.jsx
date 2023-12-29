cd2023ad5bcf8854db33dd9de06feea27be7ced9
shtanriverdi <shtanriverdi@yahoo.com>
2023-12-29 22:36:16 +0300

    Add filtering search

--- src/ThinkingInReact/ProductTable.jsx
+++ src/ThinkingInReact/ProductTable.jsx
@@ -1,16 +1,30 @@
 import ProductRow from "./ProductRow";
 import ProductCategoryRow from "./ProductCategoryRow";
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
     // Put Product Categor Header
     if (product.category !== lastCategory) {
-      rows.push(<ProductCategoryRow key={product.category} category={product.category} />);
+      rows.push(
+        <ProductCategoryRow
+          key={product.category}
+          category={product.category}
+        />
+      );
     }
-    rows.push(<ProductRow key={product.name} product={product} />);
+
+    if (product.name.toLowerCase().startsWith(filterText.toLowerCase())) {
+      if (inStockOnly && product.stocked) {
+        rows.push(<ProductRow key={product.name} product={product} />);
+      } else if (!inStockOnly) {
+        rows.push(<ProductRow key={product.name} product={product} />);
+      }
+    }
+
+
     lastCategory = product.category;
   });
 
