8522e115b58e4aa4b74e6d6c9d25f477dad6a4d4
shtanriverdi <shtanriverdi@yahoo.com>
2024-01-08 13:43:22 +0300

    Curly Done

--- src/DescribingTheUI/JSXWithCurrlyBraces/Curly.jsx
+++ src/DescribingTheUI/JSXWithCurrlyBraces/Curly.jsx
@@ -1,21 +1,32 @@
 const today = new Date();
 
 function formatDate(date) {
-  return new Intl.DateTimeFormat(
-    'en-US',
-    { weekday: 'long' }
-  ).format(date);
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
 }
 
 export default function CurlyBraces() {
   const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
   const description = "Someone";
-  const name = "Genesis";
+  
+  const person = {
+    name: "Genesis",
+    theme: {
+      backgroundColor: 'lightgreen',
+      color: 'blue'
+    }
+  };
+
   return (
     <>
       <h1>Today is {formatDate(today)}</h1>
-      <h2>Melo {name}</h2>
+      <h2>Melo {person.name}</h2>
       <img className="avatar" src={avatar} alt={description} />
+
+      <ul style={person.theme}>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
     </>
   );
 }
