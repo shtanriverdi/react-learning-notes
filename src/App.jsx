a93856ab68249c06912462c338c0a3603fb7ebe3
shtanriverdi <shtanriverdi@yahoo.com>
2024-01-08 09:07:49 +0300

    Initialize DescribingTheUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import PassingStateDown from "./QuickStart/PassingStateDown";
-import FilterableProductTable from "./ThinkingInReact/FilterableProductTable";
+// import FilterableProductTable from "./ThinkingInReact/FilterableProductTable";
 // import Game from "./TicTacToe/Game";
-import products from './ThinkingInReact/data';
+// import products from './ThinkingInReact/data';
+import Profile from './DescribingTheUI/your-first-component/App';
 
 export default function App() {
   return (
     // <PassingStateDown />
     // <Game />
-    <FilterableProductTable products={products} />
+    // <FilterableProductTable products={products} />
+    <Profile />
   );
 }
---
+++ src/DescribingTheUI/your-first-component/App.jsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+  const style = {
+    width: "100px",
+    height: "100px",
+  };
+
+  return (
+    <img
+      style={style}
+      src="https://i.imgur.com/tCRRB0v.jpeg"
+      alt="Katherine Johnson"
+    />
+  );
+}
